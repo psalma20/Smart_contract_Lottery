4503c4b225a10c3938b471ef95ab69a19c8e1b47
psalma20 <98271452+psalma20@users.noreply.github.com>
2023-04-09 19:29:45 +0200

    Commit 3
    Lottery.sol done

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,13 +1,25 @@
 //SPDX-License-Identifier:MIT
+//Blockchain is a deterministic system
+//Getting atrue  Random number we are going to have  alook outside the blockchain
+//cant use any api for obtaining any random number
+///////////////////////////////////////////////////////////////////////
+//ETH ->Pay some ETH/transaction gas to pay the smartcontract platform a littlebit of ETH for performing a transaction
+//LINK->"For smartcontract that operate with an oracle" We pay using LINK gas/ oracle gas to pay the oracle for providing data or some type of external computation for a smart contract
+
 pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+//For the onlyOwner fxn
 import "@openzeppelin/contracts/access/Ownable.sol";
+//For random Number
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract Lottery is Ownable {
+contract Lottery is VRFConsumerBase, Ownable {
     // what fxns must we will use
 
     address payable[] public players;
+    address payable public recentWinner;
+    uint256 public randomness;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
     //This means that we have a new type called lottery state with 3 positions
@@ -21,22 +33,37 @@ contract Lottery is Ownable {
     }
 
     LOTTERY_STATE public lottery_state;
+    uint256 public fee;
+    bytes32 public keyhash;
 
-    constructor(address _priceFeedAddress) public {
+
+    //VRFConsumerBase paramters will be VRF consumeraddress,linktoken address
+    //RandomContract wants a fee and keyHash
+
+    constructor(
+        address _priceFeedAddress,
+        address _vrfCoordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyhash
+    ) public VRFConsumerBase(_vrfCoordinator, _link) {
         //Need to convert $50 to $50 in ETH
         usdEntryFee = 50 * (10 ** 18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
         //AS LOTTERY_STATE are represented by NUMBERS we can easily state them aslo by
         //lottery_state=1(CLOSED)
         lottery_state = LOTTERY_STATE.CLOSED;
+        fee = _fee;
+        keyhash = _keyhash;
     }
 
     function enter() public payable {
         //$50 min for any player to enter the lottery
-        require(msg.value >= getEntranceFee(), "Not Enough ETH!!");
+       
         require(lottery_state == LOTTERY_STATE.OPEN);
+        require(msg.value >= getEntranceFee(), "Not Enough ETH!!");
 
-        players.push(msg.sender);
+        players.push(payable(msg.sender));
     }
 
     function getEntranceFee() public view returns (uint256) {
@@ -68,16 +95,39 @@ contract Lottery is Ownable {
         /////////////////////////
         //1. This Vunrable method cant be used by any production used cases
         //insecure variables will do that they will use a globally varaible(ex: msg.sender /msg.value)and hash it
-    
+
         // uint256(
         //     keccak256(
         //         abi.encodePacked(
-        //             nonce,// nonce is predictable 
-        //             msg.sender,// msg.sender is predictable 
-        //             block.difficulty,// can be manipulated by the minners gives the miners to win the lottery 
-        //             block.timestamp//Timestamp is predictable 
+        //             nonce,// nonce is predictable
+        //             msg.sender,// msg.sender is predictable
+        //             block.difficulty,// can be manipulated by the minners gives the miners to win the lottery
+        //             block.timestamp//Timestamp is predictable
         //         )
         //     )
         // ) % players.length;
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+        bytes32 requestId = requestRandomness(keyhash, fee);
     }
+    //Internal because only the VRF coordinator can return the fxn 
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness ) internal override{
+        require(lottery_state == LOTTERY_STATE.CALCULATING_WINNER, "You arent there Yet!");
+        require(_randomness >0, "random-not-found");
+        uint256 indexOfWinner=_randomness % players.length;
+        recentWinner =players[indexOfWinner];
+
+        //7 players 
+        //random number:22
+        // 22 % 7 = 1
+
+        // the recentWinner will take all money added while entering the game 
+        recentWinner.transfer(address(this).balance );
+
+        //Reseting 
+        players = new address payable [] (0);
+        lottery_state=LOTTERY_STATE.CLOSED;
+        randomness=_randomness;
+    }
+    
+
 }
